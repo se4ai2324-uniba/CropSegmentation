d9affd7fc2f98cdf664039b364685f8397798154
Midorilly <angelica.ghizzota@gmail.com>
2023-10-10 20:25:24 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The system is designed to segment crops from the background in images collected by Unmanned Aerial Vehicles',
+    author='CS-TEAM',
+    license='MIT',
+)
