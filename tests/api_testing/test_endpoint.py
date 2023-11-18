6529dcc87e49f550140d4aedfcc6441a407a50fe
mariangelapanunzio <panunziomar@gmail.com>
2023-11-18 17:09:35 +0100

    Update API testing

--- tests/api_testing/test_endpoint.py
+++ tests/api_testing/test_endpoint.py
@@ -1,33 +1,12 @@
 import os
-import sys
 from sys import platform
-if platform == 'win32':
-    sys.path.append('\\'.join(os.getcwd().split('\\')[:-2])+'\src')
-    sys.path.append('\\'.join(os.getcwd().split('\\')[:-2])+'\src\models')
-    sys.path.append('\\'.join(os.getcwd().split('\\')[:-2])+'\src\models\model')
-    sys.path.append('\\'.join(os.getcwd().split('\\')[:-2])+'\src\\api\\temp')
-else:
-    sys.path.append('/'.join(os.getcwd().split('/')[:-2])+'/src')
-    sys.path.append('/'.join(os.getcwd().split('/')[:-2])+'/src/models')
-    sys.path.append('/'.join(os.getcwd().split('/')[:-2])+'/src/models/model')
-
 import pytest
-from skimage import io
-from sklearn.metrics import jaccard_score
-#from src.models.predict_model import make_predictions, get_saved_model
-import numpy as np
-from typing import Union
-#from src.config import get_global_config
-
-from fastapi import FastAPI, status
-from fastapi.testclient import TestClient
-from fastapi.responses import FileResponse
 from src.api.server import app
+from fastapi.testclient import TestClient
 
 BASE_PATH = '\\'.join(os.getcwd().split('\\')[:-2]) + '\\' if platform == 'win32' else '/'.join(os.getcwd().split('/')[:-2]) + '/'
-print(BASE_PATH)
 client = TestClient(app)
-   
+
 
 def test_get_main():
     response = client.get('/')
@@ -51,6 +30,7 @@ def test_get_main():
 			'DagsHub': 'https://dagshub.com/se4ai2324-uniba/CropSegmentation',
 			'Authors': ['Eleonora Ghizzota', 'Mariangela Panunzio', 'Katya Trufanova', 'Alberto G. Valerio']}
 
+
 @pytest.mark.parametrize('limit', [2, 5, 10, 20, 0])
 def test_get_samples(limit):
     response = client.get('/images?limit='+str(limit))
@@ -60,7 +40,8 @@ def test_get_samples(limit):
         assert len(response.json()['samples']) == limit
     else:
         assert len(response.json()['samples']) == 102
-    
+
+
 '''
 Since the images in /temp/ are selected randomly, it is not possible to test whether a specific image is available or not.
 With this test we make sure that, given wrong image names, a 404 error is returned.
@@ -70,3 +51,16 @@ def test_get_image(image_name):
     response = client.get('/temp/{image_name}')
     assert response.status_code == 404
     assert response.request.method == "GET"
+
+
+@pytest.mark.parametrize('image_name', ['00045.jpg', '00183.jpg'])
+def test_post_predict(image_name):
+
+    client.get('/images?limit=0')
+
+    # Simulate a POST request to the /predict endpoint
+    response = client.post('/predict', json={'og_name': image_name})
+
+    # Assertions based on your expected behavior
+    assert response.status_code == 200
+    assert "mask" in response.json()
