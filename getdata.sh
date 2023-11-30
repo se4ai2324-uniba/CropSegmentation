f826c4ab0dac71a8cb89cfe5aeffb4e699c28fb5
Alberto G. Valerio <albertogvalerio@gmail.com>
2023-11-30 18:52:40 +0100

    Trigger Action

---
+++ getdata.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+wget -P data/raw https://cdn.albertovalerio.com/datasets/crop_segmentation/SE4AI/datasets.zip
+wget -P data/processed https://cdn.albertovalerio.com/datasets/crop_segmentation/SE4AI/datasets_processed.zip
+wget -P models/saved https://cdn.albertovalerio.com/datasets/crop_segmentation/SE4AI/sd_10_50_0.001_0.25_unet_model.pth
+
+unzip data/raw/datasets.zip -d data/raw/datasets
+unzip data/processed/datasets_processed.zip -d data/processed/datasets_processed
