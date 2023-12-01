e893c265e215b119322b5608dc05ee0ebbc459f3
Alberto G. Valerio <albertogvalerio@gmail.com>
2023-12-01 10:00:41 +0100

    Remove useless artifacts

--- tests/model_training_testing/test_device_training.py
+++ tests/model_training_testing/test_device_training.py
@@ -11,39 +11,61 @@ TRAIN_DATA_PATH = BASE_PATH + config.get('PROCESSED_TRAINING_DATA_PATH')
 TRAIN_LABELS_PATH = BASE_PATH + config.get('PROCESSED_TRAINING_LABELS_PATH')
 SAVED_MODEL_PATH = BASE_PATH + config.get('SAVED_MODEL_PATH')
 
-
 def test_device_training():
+	num_epochs=1
+	batch_size=config.get('BATCH_SIZE')
+	init_lr=config.get('INIT_LR')
+	ratio=.01
+	pth = (str(int(num_epochs)) + '_' + str(int(batch_size)) +
+		'_' + str(init_lr) + '_' + str(ratio)) + '_unet_model.pth'
+
 	# Assert training on CPU
 	loss_history_cpu, _, _ = train(
-		num_epochs=1,
-		ratio=.011,
+		num_epochs=num_epochs,
+		batch_size=batch_size,
+		init_lr=init_lr,
+		ratio=ratio,
 		device='cpu',
 		train_data_path=TRAIN_DATA_PATH,
 		train_labels_path=TRAIN_LABELS_PATH,
 		saved_model_path=SAVED_MODEL_PATH)
 	assert loss_history_cpu
 
+	if os.path.isfile(SAVED_MODEL_PATH + pth):
+		os.unlink(SAVED_MODEL_PATH + pth)
+
 	# If CUDA is available, assert training on CUDA
 	if torch.cuda.is_available():
 		loss_history_cuda, _, _ = train(
-			num_epochs=1,
-			ratio=.012,
+			num_epochs=num_epochs,
+			batch_size=batch_size,
+			init_lr=init_lr,
+			ratio=ratio,
 			device='cuda',
 			train_data_path=TRAIN_DATA_PATH,
 			train_labels_path=TRAIN_LABELS_PATH,
 			saved_model_path=SAVED_MODEL_PATH)
 		assert loss_history_cuda
 
+	if os.path.isfile(SAVED_MODEL_PATH + pth):
+		os.unlink(SAVED_MODEL_PATH + pth)
+
 	# If MPS (MacOs) is available, assert training on MPS
 	if torch.backends.mps.is_available():
 		loss_history_mps, _, _ = train(
-			num_epochs=1,
-			ratio=.013,
+			num_epochs=num_epochs,
+			batch_size=batch_size,
+			init_lr=init_lr,
+			ratio=ratio,
 			device='mps',
 			train_data_path=TRAIN_DATA_PATH,
 			train_labels_path=TRAIN_LABELS_PATH,
 			saved_model_path=SAVED_MODEL_PATH)
 		assert loss_history_mps
+
+	if os.path.isfile(SAVED_MODEL_PATH + pth):
+		os.unlink(SAVED_MODEL_PATH + pth)
+
 
 if __name__ == "__main__":
 	pytest.main()
--- tests/model_training_testing/test_training_completion.py
+++ tests/model_training_testing/test_training_completion.py
@@ -8,27 +8,42 @@ BASE_PATH = '\\'.join(os.getcwd().split('\\')[:-2]) + '\\' if platform == 'win32
 config = get_global_config()
 TRAIN_DATA_PATH = BASE_PATH + config.get('PROCESSED_TRAINING_DATA_PATH')
 TRAIN_LABELS_PATH = BASE_PATH + config.get('PROCESSED_TRAINING_LABELS_PATH')
+SAVED_MODEL_PATH = BASE_PATH + config.get('SAVED_MODEL_PATH')
 
 
 def test_training_completion():
-    # Capture the model_save_path return value
-    loss_history, final_lr, model_save_path = train(
-        num_epochs=5,
-        ratio=.014,
-        train_data_path=TRAIN_DATA_PATH,
-        train_labels_path=TRAIN_LABELS_PATH
-    )
+	num_epochs=5
+	batch_size=config.get('BATCH_SIZE')
+	init_lr=config.get('INIT_LR')
+	ratio=.01
+	pth = (str(int(num_epochs)) + '_' + str(int(batch_size)) +
+		'_' + str(init_lr) + '_' + str(ratio)) + '_unet_model.pth'
 
-    # Assert that the training has likely completed by checking the final learning rate
-    min_learning_rate = 0.00001  # Define a minimum learning rate threshold
-    assert final_lr >= min_learning_rate
+	# Capture the model_save_path return value
+	loss_history, final_lr, model_save_path = train(
+		num_epochs=num_epochs,
+		batch_size=batch_size,
+		init_lr=init_lr,
+		ratio=ratio,
+		train_data_path=TRAIN_DATA_PATH,
+		train_labels_path=TRAIN_LABELS_PATH,
+		saved_model_path=SAVED_MODEL_PATH
+	)
 
-    # Assert that the loss history is not empty, implying training iterations occurred
-    assert loss_history['train_loss']
-    assert loss_history['val_loss']
+	# Assert that the training has likely completed by checking the final learning rate
+	min_learning_rate = 0.00001  # Define a minimum learning rate threshold
+	assert final_lr >= min_learning_rate
 
-    # New assertion to check if the model file has been saved
-    assert os.path.exists(model_save_path), f"Model file not found at {model_save_path}"
+	# Assert that the loss history is not empty, implying training iterations occurred
+	assert loss_history['train_loss']
+	assert loss_history['val_loss']
+
+	# New assertion to check if the model file has been saved
+	assert os.path.exists(model_save_path), f"Model file not found at {model_save_path}"
+
+	if os.path.isfile(SAVED_MODEL_PATH + pth):
+		os.unlink(SAVED_MODEL_PATH + pth)
+
 
 if __name__ == "__main__":
     pytest.main()
