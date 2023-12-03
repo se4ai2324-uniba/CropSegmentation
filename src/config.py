bbaf13ea208f6039fc6a8b5f1d35eaa1d67e903b
Alberto G. Valerio <albertogvalerio@gmail.com>
2023-12-03 10:01:34 +0100

    Update dvc config

--- src/config.py
+++ src/config.py
@@ -38,7 +38,7 @@ CONFIG = {
 	'RATIO':							.25,
 	'PARAMS_SEARCH':					{
 											'BATCH_SIZE':	[25, 50],
-											'INIT_LR':		[0.1, 0.01, 0.001, 0.0001, 0.0005, 0.00001]
+											'INIT_LR':		[0.001, 0.0001, 0.0005]
 										},
 	'EMISSION_FILE_PATH':				'reports/carbon/emissions.csv',
 	'TRACK_CO2':						0,
@@ -48,7 +48,7 @@ CONFIG = {
 	'METRICS_BASE_PATH':				'models/metrics/',
 
 	# server.py
-	'BEST_MODEL_PATH':					'models/saved/sd_10_50_0.001_0.25_unet_model.pth',
+	'BEST_MODEL_PATH':					'models/saved/sd_10_50_0.0001_0.25_unet_model.pth',
 	'TEMP_PATH':						'src/api/temp/'
 }
 
